e3270731259d1cdb9a64cb20b6e36116c06b1292
karlmarx81 <35492926+karlmarx81@users.noreply.github.com>
2018-01-22 23:24:22 +0900

    Script Commit

---
+++ MyUnityProject/MyUnityProj_01/Assets/Scripts/CarController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+	public List<AxleInfo> axleInfos;
+
+	public float maxDriveTorque;
+	public float maxBrakeTorque;
+	public float maxSteering;
+
+	//Rigidbody thisRb;
+
+	// Use this for initialization
+	void Start () {
+		//thisRb = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float accel = maxDriveTorque * Input.GetAxis ("Vertical") * Time.fixedDeltaTime;
+		float brake = Input.GetAxis("Vertical") < 0 ? maxBrakeTorque * Input.GetAxis ("Vertical") * Time.fixedDeltaTime : 0f;	
+		float angular = maxSteering * Input.GetAxis ("Horizontal") * Time.fixedDeltaTime;
+
+
+		for (int i = 0; i < axleInfos.Count; i++) {
+			if (axleInfos [i].isDriveWheel == true) {
+				
+				axleInfos [i].leftWheel.motorTorque = accel + brake;
+				axleInfos [i].rightWheel.motorTorque = accel + brake;
+			}
+
+			if (axleInfos [i].isSteeringWheel == true) {
+				axleInfos [i].leftWheel.steerAngle = angular;
+				axleInfos [i].rightWheel.steerAngle = angular;
+			}
+		}
+	}
+}
+
+[System.Serializable]
+public class AxleInfo
+{
+	public WheelCollider leftWheel;
+	public WheelCollider rightWheel;
+	public bool isDriveWheel;
+	public bool isSteeringWheel;
+}
