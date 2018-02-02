30486fc2ecb0e912425bd481f40aa82b987ea7aa
karlmarx81 <35492926+karlmarx81@users.noreply.github.com>
2018-02-02 23:57:24 +0900

    커스텀 인스펙터 기능 추가

---
+++ MyUnityProject/MyUnityProj_01/Assets/Scripts/AdjustCube.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustCube : MonoBehaviour {
+
+	void Start () {
+
+	}	
+
+	void Update () {
+		
+	}
+
+	void OnDrawGizmos()
+	{
+//		Gizmos.color = Color.red;
+//		Gizmos.DrawRay (transform.position, -transform.up);
+//
+//		Gizmos.color = Color.blue;
+//		Ray ray = new Ray (transform.position, transform.up * -1f);
+//		RaycastHit hit;
+//
+//		if (Physics.Raycast (ray, out hit)) {
+//			Gizmos.DrawRay (transform.position, hit.normal);
+//		}
+	}
+
+	public void ChangePosNRot()
+	{
+		transform.position = transform.position + transform.up * 5f;
+
+		Ray ray = new Ray (transform.position, transform.up * -1f);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.transform.gameObject.CompareTag ("Ground")) {
+				Vector3 position = hit.point;
+				Vector3 direction = hit.normal;
+
+				transform.position = position;
+				transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+
+				//Debug.Log (transform.name + ", hit Name: " + hit.transform.name + ", hit point: " + hit.point + ",hit normal: " + hit.normal);
+			}
+		}
+	}
+}
---
+++ MyUnityProject/MyUnityProj_01/Assets/Scripts/AdjustCubeIns.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AdjustCube))]
+[CanEditMultipleObjects]
+public class AdjustCubeIns : Editor {
+
+	void OnEnable()
+	{		
+	}
+
+	public override void OnInspectorGUI()
+	{
+		if (GUILayout.Button ("Adjust Pos/Rot")) {
+			foreach (var obj in Selection.gameObjects) {
+				AdjustCube objScript = obj.GetComponent<AdjustCube> ();						
+				objScript.ChangePosNRot ();
+			}
+		}	
+	}
+}
