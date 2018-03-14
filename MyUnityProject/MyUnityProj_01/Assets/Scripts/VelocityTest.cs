be769697a12fe706a43a1c41dcb7a2e320b6985d
karlmarx81 <35492926+karlmarx81@users.noreply.github.com>
2018-03-14 15:26:12 +0900

    속도 테스트 씬 추가

---
+++ MyUnityProject/MyUnityProj_01/Assets/Scripts/VelocityTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VelocityTest : MonoBehaviour {
+
+	public float velocityMag;
+	public Vector3 applyDirection;
+
+	Rigidbody thisRb;
+	Vector3 initPos;
+
+	void Start () {
+		thisRb = GetComponent<Rigidbody> ();
+		initPos = this.transform.position;
+	}	
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			thisRb.velocity = applyDirection.normalized * velocityMag;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			thisRb.velocity = Vector3.zero;
+			transform.position = initPos;
+			transform.rotation = Quaternion.identity;
+		}
+	}
+}
